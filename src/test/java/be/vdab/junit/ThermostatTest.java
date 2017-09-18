0557a8785b0b0bb81775f36c817335702ec4e7c6
Radek <radoslaw.lizak@gmail.com>
2017-09-18 12:17:19 +0200

    class Thermostat class ThermostatTest

---
+++ src/test/java/be/vdab/junit/ThermostatTest.java
@@ -0,0 +1,16 @@
+package be.vdab.junit;
+
+import be.vdab.unit.Heating;
+
+public class ThermostatTest {
+
+    //GENESTE KLASSE HEATINGSTUB:
+
+    class HeatingStub implements Heating {
+        public void setHeating(boolean status) {
+            //Empty implementation
+        }
+    }
+
+
+}
