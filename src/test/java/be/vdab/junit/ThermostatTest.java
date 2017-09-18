c1c9b1f6da2747b1e68f85aad0c241c2d47299e6
Radek <radoslaw.lizak@gmail.com>
2017-09-18 12:34:45 +0200

    class ThermostatTest temp

--- src/test/java/be/vdab/junit/ThermostatTest.java
+++ src/test/java/be/vdab/junit/ThermostatTest.java
@@ -1,16 +1,54 @@
 package be.vdab.junit;
 
 import be.vdab.unit.Heating;
+import be.vdab.unit.Temperature;
+import be.vdab.unit.Thermostat;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class ThermostatTest {
 
-    //GENESTE KLASSE HEATINGSTUB:
+//    GENESTE KLASSE HEATINGSTUB:
+//    FAKE OBJECT
 
     class HeatingStub implements Heating {
         public void setHeating(boolean status) {
             //Empty implementation
         }
     }
+
+    private Thermostat thermostat;
+
+    @Before
+    public void init() {
+        thermostat = new Thermostat(new HeatingStub());
+    }
+
+    //setCurrentTemperature
+    @Test
+    public void testChangeCurrentTemperature() {
+        Assert.assertTrue(thermostat.setCurrentTemperature(0);
+
+    @Test
+    //setTargetTemperature
+    public void testChangeTargetTemperature() {
+
+    }
+
+    @Test
+   //isHeating
+    public void testHeating() {
+
+    }
+
+    @Test
+    //evaluate
+    public void testEvaluate() {
+
+    }
+
+
 
 
 }
